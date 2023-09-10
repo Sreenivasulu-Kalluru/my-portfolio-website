93a2ee4d012b7e80771a06cd261a43a876fcc924
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-09-10 16:47:57 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+const servicesData = [
+  {
+    title: 'Frontend Development',
+    category: 'frontend',
+    description: [
+      '1. The front end is the part of the website users can see and interact with such as the graphical user interface (GUI) and the command line including the design, navigating menus, texts, images, videos, etc. The backend, on the contrary, is part of the website users cannot see and interact with. ',
+      '2. The visual aspects of the website that can be seen and experienced by users are frontend. On the other hand, everything that happens in the background can be attributed to the backend.',
+    ],
+  },
+  {
+    title: 'Backend Development',
+    category: 'backend',
+    description: [
+      '1. The backend is the server side of the website. It stores and arranges data, and also makes sure everything on the client side of the website works fine. It is part of the website that you cannot see and interact with. It is the portion of software that does not come in direct contact with the users.',
+      '2. The parts and characteristics developed by backend designers are indirectly accessed by users through a front-end application. Activities, like writing APIs, creating libraries, and working with system components without user interfaces or even systems of scientific programming, are also included in the backend.',
+    ],
+  },
+  {
+    title: 'Full Stack Development',
+    category: 'fullstack',
+    description: [
+      '1. Full Stack Development is the end-to-end development of applications. It includes both the front end and back end of an application. The front end is usually accessed by a client, and the back end forms the core of the application where all the business logic is applied',
+      '2. Full Stack Development, as the name suggests, is the combination of both front-end and back-end portions in the life of a web application. ',
+    ],
+  },
+];
---
+++ index.js
@@ -0,0 +1,141 @@
+// * SERVICES TOGGLE
+const servicesButtons = document.querySelectorAll('.service__item');
+const serviceDetails = document.querySelector('.services__right');
+// console.log(servicesButtons);
+
+const getService = (category) => {
+  const details = servicesData.find((item) => item.category === category);
+  console.log(details);
+  serviceDetails.innerHTML = `
+      <h3>${details.title}</h3>
+      ${details.description
+        .map((paragraph) => '<p>' + paragraph + '</p>')
+        .join('')}
+  `;
+};
+
+const removeActiveClass = () => {
+  servicesButtons.forEach((button) => {
+    button.classList.remove('active');
+  });
+};
+
+servicesButtons.forEach((item) => {
+  item.addEventListener('click', () => {
+    removeActiveClass();
+    const serviceClass = item.classList[1];
+    // console.log(serviceClass);
+    getService(serviceClass);
+    item.classList.add('active');
+  });
+});
+
+getService('frontend');
+
+// * MIXITUP - Projects Section
+const containerEl = document.querySelector('.projects__container');
+var mixer = mixitup(containerEl, {
+  animation: {
+    enable: false,
+  },
+});
+
+mixer.filter('*');
+
+// * SWIPER JS - Testimonials Section
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// * NAV TOGGLE
+const navMenu = document.querySelector('.nav__menu');
+const navOpenBtn = document.querySelector('.nav__toggle-open');
+const navCloseBtn = document.querySelector('.nav__toggle-close');
+
+const openNavHandler = () => {
+  navMenu.style.display = 'flex';
+  navOpenBtn.style.display = 'none';
+  navCloseBtn.style.display = 'inline-block';
+};
+
+const closeNavHandler = () => {
+  navMenu.style.display = 'none';
+  navOpenBtn.style.display = 'inline-block';
+  navCloseBtn.style.display = 'none';
+};
+
+navOpenBtn.addEventListener('click', openNavHandler);
+navCloseBtn.addEventListener('click', closeNavHandler);
+
+// * Close Nav Menu on click of nav link on small screens i.e, < 780px
+const navItems = navMenu.querySelectorAll('a');
+
+if (window.innerWidth < 768) {
+  navItems.forEach((item) => {
+    item.addEventListener('click', closeNavHandler);
+  });
+}
+
+// * DARK & LIGHT THEME TOGGLE FEATURE
+const themeBtn = document.querySelector('.nav__theme-btn');
+
+themeBtn.addEventListener('click', () => {
+  let bodyClass = document.body.className;
+  if (!bodyClass) {
+    bodyClass = 'dark';
+    document.body.className = bodyClass;
+    // * change toggle icon
+    themeBtn.innerHTML = '<i class="uil uil-sun"></i>';
+    // * save theme to local storage
+    // window.localStorage.setItem('theme', bodyClass);
+  } else {
+    bodyClass = '';
+    document.body.className = bodyClass;
+    // * change toggle icon
+    themeBtn.innerHTML = '<i class="uil uil-moon"></i>';
+    // * save theme to local storage
+    // window.localStorage.setItem('theme', bodyClass);
+  }
+});
+
+// * load theme on page load initially
+window.addEventListener('load', () => {
+  document.body.className = window.localStorage.getItem('theme');
+});
+
+// * Custom Mouse Cursor
+const cursorDot = document.querySelector('[data-cursor-dot]');
+const cursorOutline = document.querySelector('[data-cursor-outline]');
+
+window.addEventListener('mousemove', (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 100, fill: 'forwards' }
+  );
+});
