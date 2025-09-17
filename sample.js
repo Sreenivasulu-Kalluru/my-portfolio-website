7379a497d0d9f4e70b6db4283ba3975d2ac02a10
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2025-09-17 14:36:16 +0530

    My Tech Stack headline updated

---
+++ sample.js
@@ -0,0 +1,15 @@
+greet();
+print();
+
+// let name = 'Vasu';
+
+var sName = 'Ram';
+
+function greet() {
+  // console.log(name);
+  console.log(sName);
+}
+
+const print = function () {
+  console.log(name);
+};
